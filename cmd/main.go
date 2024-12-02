28d1a5248a1f59d7adf847699fe415a6a16dd4fc
SwenWebber <limpo2059@gmail.com>
2024-12-02 13:34:18 +0500

    adding new files

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,34 @@
-package cmd
+package main
 
-func main(){
+import (
+	"math/rand"
 
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
+func main() {
+	e := echo.New()
+
+	//Middleware
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	
+
+	
 }
 
-func GenerateString(size int) string{
+func GenerateString(size int) string {
 
-	const charset = "ab"
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	b := make([]byte, size)
+
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
 
 }
